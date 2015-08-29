bfe0bfa083b3b4e90d0f54e0b5d8a1012d7b4395
Paul Klenck <pklenck@gmail.com>
2015-08-28 19:01:31 -0600

    multiple small code changes

--- rps.js
+++ rps.js
@@ -1,13 +1,7 @@
 //Get users choice
-
 var userChoice = prompt("Do you choose rock, paper, or scissors?");
-
 //Have the computer make a choice. Value from 0 - 1
-
-var computerChoice = Math.random();
-
-//Determine if the computer's choice is rock, paper, or scissors
-
+var computerChoice = Math.random(); //Determine if the computer's choice is rock, paper, or scissors
 if (computerChoice<=0.33) {
   computerChoice="rock";
 }else if (computerChoice<=0.66) {
@@ -15,9 +9,7 @@ if (computerChoice<=0.33) {
 }else {
   computerChoice = "scissors";
 }
-
 //Create a function called compare to see if the choices match
-
 var compare = function(choice1, choice2) {
   if (choice1 == choice2)  {
     return "It's a tie!"; //both choices are the same - all done!
@@ -25,15 +17,15 @@ var compare = function(choice1, choice2) {
     if (choice2 == "scissors") {
       return "You win! Rocks crushes scissors!"; //
     } else {
-      return "You lose! Paper covers rock" //choice1 is rock, choice2 is paper
-      }
+        return "You lose! Paper covers rock" //choice1 is rock, choice2 is paper
+           }
     }
 
 else if(choice1 == "paper") {
-  if (choice2 == "rock") {
+  if(choice2 == "rock") {
     return "You win! Paper covers Rock!";
   } else {
-    return "You lose! Scissors cuts paper";
+      return "You lose! Scissors cuts paper";
   }
 }
 
